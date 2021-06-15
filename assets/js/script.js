3655a45a1a8b52c67000a903d50a1935aebb6969
josephWoodland <78616045+josephWoodland@users.noreply.github.com>
2021-06-15 20:27:31 +0100

    Added the function to change the state of the play button on page load

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,39 @@
+"use strict";
 
-console.log('hello world');
+const video = document.querySelector("#myVideo");
+const play = document.querySelector(".form__video-play");
 
-$(document).ready(function() {
-    let state = "paused";
-    $('#pause').on('click', function() {
-      if(state == 'paused') {
-        state = "playing";
-        $("#circle").attr("class", "play");
-        $("#from_pause_to_play")[0].beginElement();
-      } else {
-        state = "paused";
-        $("#circle").attr("class", "");
-        $("#from_play_to_pause")[0].beginElement();
-      }
-    });
+let state = "paused";
+
+const playing = function () {
+  $(document).ready(function () {
+    console.log("Please work!!");
+    if (state == "paused") {
+      state = "playing";
+      $("#circle").attr("class", "play");
+      $("#from_pause_to_play")[0].beginElement();
+    } else {
+      state = "paused";
+      $("#circle").attr("class", "");
+      $("#from_play_to_pause")[0].beginElement();
+    }
   });
+};
+
+$(document).ready(function () {
+  $("#pause").on("click", function () {
+    if (state === "paused") {
+      state = "playing";
+      video.pause();
+      $("#circle").attr("class", "");
+      $("#from_pause_to_play")[0].beginElement();
+    } else {
+      state = "paused";
+      video.play();
+      $("#circle").attr("class", "play");
+      $("#from_play_to_pause")[0].beginElement();
+    }
+  });
+});
+
+playing();
